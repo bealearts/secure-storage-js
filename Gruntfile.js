48079dfeabbe220e95524c80c6633b48ad6da96a
David Beale <david.beale@bealearts.co.uk>
2013-08-11 14:15:47 +0100

    Setup Project

---
+++ Gruntfile.js
@@ -0,0 +1,87 @@
+module.exports = function(grunt) {
+
+  // Libs
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-bower-task');
+
+
+  // Temp dir
+  var TempDir = require('temporary/lib/dir');
+  var tempDir = new TempDir().path;
+
+
+  // Project configuration.
+  grunt.initConfig(
+  {
+    packkage: grunt.file.readJSON('package.json'),
+    clean: {
+      build: {
+        src: ['build']
+      }
+    }//,
+    // bower: {
+    //   build: {
+    //     options: {
+    //       targetDir: 'build/lib',
+    //       copy: true,
+    //       cleanBowerDir: true
+    //     } 
+    //   }
+    // },
+    // copy: {
+    //   build: {
+    //     files: [
+    //       {
+    //         expand: true,
+    //         cwd: 'src/',
+    //         src: ['**/.htaccess', 'next-boat/assets/**/*.*', '*.*', 'lib/**/*.*'], 
+    //         dest: 'build/'
+    //       }
+    //     ]
+    //   }
+    // },
+    // concat: {
+    //   buildJS: {
+    //     options: {
+    //       banner: '(function(){\n\n"use strict";\n\nvar module = angular.module(\'next-boat\', []);\n\n',
+    //       footer: '\n\n}.call({}));'
+    //     },
+    //     src: ['src/next-boat/**/*.js'],
+    //     dest: 'build/next-boat/next-boat.js'
+    //   },
+    //   buildCSS: {
+    //     src: ['src/next-boat/**/*.css'],
+    //     dest: 'build/next-boat/next-boat.css'
+    //   }
+    // },
+    // watch: {
+    //   main: {
+    //       files: ['*.*', 'src/**/*.*'],
+    //       tasks: 'build'
+    //   }
+    // }
+  });
+
+
+
+  // Tasks
+  grunt.registerTask('build', [
+        'clean',
+        'copy',
+        'bower',
+        'concat'
+  ]);
+
+  grunt.registerTask('deploy', [
+        'build'
+  ]);
+
+  grunt.registerTask('default', [
+        'build'
+  ]);
+
+
+};
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 secure-storage-js
 =================
 
-Pure JS implementation of SecureStorage API
+Pure JS implementation of SecureStorage API as proposed by http://www.nczonline.net/blog/2010/04/13/towards-more-secure-client-side-data-storage/
