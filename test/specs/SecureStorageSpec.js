6b2df8c4f0c659581b1b6688973af63bd5ec7197
David Beale <david.beale@bealearts.co.uk>
2013-09-09 14:51:50 +0100

    Jasmine Specs

--- test/specs/SecureStorageSpec.js
+++ test/specs/SecureStorageSpec.js
@@ -1,26 +1,51 @@
 
-require('../bower_components/cryptico/cryptico.js');
-require('../src/secure-storage.js');
-
 describe('The Secure Storage API', function() {
 	
 	var key = '';	
 	
 	beforeEach(function(){
 		key = sha256.hex('somepassword');	
+	});
+
+	afterEach(function(){
+		localStorage.removeItem('securedStore_test');
+		localStorage.removeItem('securedStore_test_locked');
 	});	
 	
 	it('can create a Secure Store', function() {
 		openSecureStorage('test', AES_256, key, function(store){
-					
+			expect(store).toBeDefined();
+
+			var value = store.getItem('aValue');
+			expect(value).toBeNull();			
 		});
+
+		var localStore = localStorage.getItem('secureStore_test');
+		expect(localStore).toBeDefined();
 	});	
 	
 	it('can open an existing Secure Store', function() {
-		openSecureStorage('test')			
+		openSecureStorage('test', AES_256, key, function(store){
+			store.setItem('aValue', 'someValue');
+		});
+
+		openSecureStorage('test', AES_256, key, function(store){
+			expect(store).toBeDefined();
+
+			var value = store.getItem('aValue');
+			expect(value).toBeDefined();
+			expect(value).toEqual('someValue');
+		});		
 	});	
 
 	it('can remove a Secure Store', function() {
-	
+		openSecureStorage('test', AES_256, key, function(store){
+			store.setItem('aValue', 'someValue');
+		});
+
+		removeSecureStorage('test');
+
+		var localStore = localStorage.getItem('secureStore_test');
+		expect(localStore).toBeNull();
 	});	
 })
